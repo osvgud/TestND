8dc3c92e2aee5e01704bcc52ce34c51e04ae9151
Osvaldas Gudauskas <gudauskas.osvaldas@gmail.com>
2018-12-07 19:12:48 +0200

    NumberFormatter klasė

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Format\NumberFormatter;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -10,8 +11,14 @@ class HomeController extends Controller
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(NumberFormatter $format)
     {
+        $numb= $format->format(12.005);
+        dump($numb);
+        $numb= $format->format(999600);
+        dump($numb);
+        $numb = $format->format(99960);
+        dump($numb);exit;
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
--- src/Format/NumberFormatter.php
+++ src/Format/NumberFormatter.php
@@ -1,12 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: osvaldas
- * Date: 18.12.7
- * Time: 18.40
- */
+
+namespace App\Format;
 
 class NumberFormatter
 {
-
+    public function format($number)
+    {
+        if (abs($number) >= 999500) {
+            return number_format($number / 1000000, 2, '.', ' ') . "M";
+        } elseif (abs($number) >= 99950) {
+            return number_format($number / 1000, 2, '.', ' ') . "K";
+        } elseif(abs($number) > 1000){
+            return number_format($number,2,'.',' ');
+        } else {
+            $whole = floor($number);
+            if($number-$whole < 0.005)
+                return number_format($number,0,'.',' ');
+            else {
+                return number_format($number, 2, '.', ' ');
+            }
+        }
+    }
 }
