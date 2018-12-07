4e90867f8c867184fcab2c82a84619295393569a
Osvaldas Gudauskas <gudauskas.osvaldas@gmail.com>
2018-12-07 20:33:11 +0200

    NumberFormatter testai bei testų pagalba pataisyta NumberFormatter klasė

--- src/Format/NumberFormatter.php
+++ src/Format/NumberFormatter.php
@@ -9,17 +9,20 @@ class NumberFormatter
         if (abs($number) >= 999500) {
             return number_format($number / 1000000, 2, '.', ' ') . "M";
         } elseif (abs($number) >= 99950) {
-            return number_format($number / 1000, 2, '.', ' ') . "K";
-        } elseif(abs($number) > 1000){
-            return number_format($number,2,'.',' ');
+            return number_format($number / 1000, 0, '.', ' ') . "K";
+        } elseif(abs($number) >= 1000){
+            return number_format($number,0,'.',' ');
         } else {
-            $whole = floor($number);
-            if($number-$whole < 0.005){
-                return number_format($number,0,'.',' ');
-            }
-            else {
-                return number_format($number, 2, '.', ' ');
-            }
+            return number_format($number, $this->spacesAfterComma(round($number,2)), '.', ' ');
         }
+    }
+
+    public function spacesAfterComma($number)
+    {
+        $whole = floor($number);
+        if($number-$whole < 0.005){
+            return 0;
+        }
+        return 2;
     }
 }
